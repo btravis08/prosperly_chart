faa6697492347b44cdaae940b4310eb2060e4264
btravis08 <brycetravis@gmail.com>
2023-02-19 01:45:26 -0800

    Update returns-cash-flow-calculator.js

--- returns-cash-flow-calculator.js
+++ returns-cash-flow-calculator.js
@@ -157,7 +157,7 @@ class ReturnsCashFlowCalculator {
         d.vacancyOverTime;
       d.totalOperatingExpensesOverTime = d.taxesOverTime + d.insuranceOverTime + d.repairsMaintenanceOverTime;
       d.netOperatingIncomeOverTime = d.grossOperatingIncomeOverTime - d.totalOperatingExpensesOverTime;
-      d.cashFlowBeforeDebt = d.grossOperatingIncomeOverTime - d.totalOperatingExpensesOverTime - d.capExOverTime;
+      d.cashFlowBeforeDebtOverTime = d.grossOperatingIncomeOverTime - d.totalOperatingExpensesOverTime - d.capExOverTime;
       d.rentalCashflowOverTime =
         d.rentalIncomeOverTime - d.totalMonthlyExpensesOverTime;
       d.totalCashflowOverTime =
