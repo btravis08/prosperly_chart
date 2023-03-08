6781e58d545ca0f0dc7241f69b2741cb25fb52d9
btravis08 <brycetravis@gmail.com>
2023-03-07 16:30:37 -0800

    Update render_charts.js

--- render_charts.js
+++ render_charts.js
@@ -88,11 +88,8 @@ window.addEventListener("load", async () => {
       console.log("data", data);
       console.log("monthlyData", monthlyData);
       console.log("otherData", otherData);
-      await Promise.all([
-  	Wized.data.setVariable("otherData", otherData),
-  	Wized.data.setVariable("data", data)
-      ]);
-
+      await Wized.data.setVariable("otherData", otherData);
+      await Wized.data.setVariable("data", data);
     	
 
 
